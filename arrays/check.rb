69640b73680412d126a79b672ac739ea8462a6c1
Carlo Kruger <ironicbuddha@gmail.com>
2018-10-24 10:40:23 +1300

    Completed arrays chapter

---
+++ arrays/check.rb
@@ -0,0 +1,7 @@
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+arr.each do |n|
+  if n == number
+    puts 'True'
+  end
+end
---
+++ arrays/deduct.rb
@@ -0,0 +1,4 @@
+first = [1, 2, 3, 4, 5]
+second = first.map { |n| n + 2 }
+p first
+p second
